f0b6a7f263c1bfc51bccd58ee044488a1dab8799
himanshusaini200 <147241235+himanshusaini200@users.noreply.github.com>
2023-10-12 04:59:37 +0000

    conversion od datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33 //inko ham number mai convert karenege to ye value ayegi//
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// inko convert karne pr inki ye value ayegi
+// 1 => true; 0 => false
+// "" => false
+// "hiamsnhu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
