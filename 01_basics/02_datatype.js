98823e4a2bbab1215aaa9b75d53e3ae2547ee521
himanshusaini200 <147241235+himanshusaini200@users.noreply.github.com>
2023-10-12 04:18:21 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all js code as a newer version
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3+3)
+
+console.log("himanshu") 
+
+let name = "himanshu"
+let age = 19
+let isloggedIn = false
+
+// number => 2 to power 53
+//null => standalone value  
+// boolean => true/false
+// bigint
+// symbol => unique
+// string => ""
+// undefined =>
+
+//object 
+
+console.log(typeof "himanshu")
