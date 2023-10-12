e3242004b2cca6f390e844930cbacb2d87d8ea3b
himanshusaini200 <147241235+himanshusaini200@users.noreply.github.com>
2023-10-12 07:13:25 +0000

    datatype-summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -34,4 +34,26 @@ const myFunction = function(){
 
 console.log(typeof anotherId);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (NoN-primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
