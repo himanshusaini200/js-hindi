69ae77fc2ead9bf9a805db2fb71e9ec1368f64eb
himanshusaini200 <147241235+himanshusaini200@users.noreply.github.com>
2023-10-16 12:17:31 +0000

    onejs

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,38 @@
-//if
-const isUserloggedIn = true 
+// //if
+// const isUserloggedIn = true 
+// const temperature = 41
 
-if (isUserloggedIn) {
-    
+// if (temperature === 40){
+// console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("execute");
+// <, >, <=, >=, ==, ===, !==
+
+
+// const score = 200
+
+// if (score > 100) {
+//     var power = "fly"
+//     console.log('User power: ${power}');
+// }
+
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+ 
+// if (balance > 500) console.log("test"), console.log("test2");
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+
+} else if (balance < 900) {
+    console.log("less than 900");
+} else if (balance < 1200) {
+    console.log("less than 1200");
 }
