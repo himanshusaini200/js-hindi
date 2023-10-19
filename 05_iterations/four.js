622f7fa73537898eaa75187b643e1958b508a7b6
himanshusaini200 <147241235+himanshusaini200@users.noreply.github.com>
2023-10-19 12:53:35 +0000

    fouriterationjs

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -3,9 +3,24 @@ const myObject = {
     cpp: 'c++',
     rb: 'ruby',
     swift: "swift by apple"
-}
+}  
 
 for (const key in myObject) {
-   console.log(`${key} shortcut is for ${myObject[key]}`);
-    
+   //console.log(`${key} shortcut is for ${myObject[key]}`);
     }
+    const programming = ["js", "rb", "py", "java", "cpp"]
+    for (const key in programming) {
+     //console.log(programming[key]);
+    }
+
+
+    // const map = new Map()
+    // map.set('IN', "India")
+    // map.set('USA', "United States Of America")
+    // map.set('Fr', "France")
+    // map.set('IN', "India")
+
+    // for (const key in  map) {
+    //    console.log(key);
+    //     }
+    
