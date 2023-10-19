2709c58fb202353334b083b3f236a88aef6529e7
himanshusaini200 <147241235+himanshusaini200@users.noreply.github.com>
2023-10-19 05:36:06 +0000

    one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -22,17 +22,31 @@
 // console.log(`User power: ${power}`);
 
 
-const balance = 1000
+// const balance = 1000
  
 // if (balance > 500) console.log("test"), console.log("test2");
 
-if (balance < 500) {
-    console.log("less than 500");
-} else if (balance < 750) {
-    console.log("less than 750");
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
 
-} else if (balance < 900) {
-    console.log("less than 900");
-} else if (balance < 1200) {
-    console.log("less than 1200");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else if (balance < 1200) {
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
 }
